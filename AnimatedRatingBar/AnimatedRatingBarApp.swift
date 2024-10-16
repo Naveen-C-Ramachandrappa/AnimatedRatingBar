d37a55ae699fd6029df879a325fed1c65fbe856c
NaveenRam <accessnaveen@gmail.com>
2024-10-16 12:11:18 -0500

    Initial Commit

---
+++ AnimatedRatingBar/AnimatedRatingBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedRatingBarApp.swift
+//  AnimatedRatingBar
+//
+//  Created by Naveen Ramachandrappa on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedRatingBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
